3daa257e77e2a6bc00073c7f8ab37c9b947276a6
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2013-02-08 08:23:12 +0100

    Minor modifications

--- src/main/scala/File/Locations.scala
+++ src/main/scala/File/Locations.scala
@@ -1,16 +1,12 @@
 package file
 
-import util.MongoCollections
 import com.mongodb.casbah.Imports._
 
 
 object Locations {
 
-  case class Locations(db: MongoDB) {
-    def findByArtistName(artistName: String) = {
-      db(MongoCollections.locations.name()).findOne(MongoDBObject("artistName" -> artistName))
-    }
+  def byArtistName(artistName: String): MongoDBObject = {
+    MongoDBObject("artistName" -> artistName)
   }
-
 }
 
